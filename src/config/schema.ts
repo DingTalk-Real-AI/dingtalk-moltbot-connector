ziyilin/dingtalk-openclaw-connector

feat: add AI Card feedback callback with configurable like/dislike
Add TOPIC_CARD Stream callback handler for custom AI Card templates,
supporting like/dislike feedback. Introduces 3 optional config fields
(cardLikeActionId, cardDislikeActionId, cardLikeVar) so different
card templates can customize callback action IDs and variable names.

src/config/schema.ts
@@ -97,6 +97,11 @@ const DingtalkSharedConfigShape = {
   groupSessionScope: GroupSessionScopeSchema,
   asyncMode: z.boolean().optional(),
   ackText: z.string().optional(),
+  cardTemplateId: z.string().optional(), // Custom AI Card template ID (default: built-in template)
+  cardTemplateKey: z.string().optional(), // Content variable name in the card template (default: "msgContent")
+  cardLikeActionId: z.string().optional(), // Like button callback action ID (default: "ai_res_like")
+  cardDislikeActionId: z.string().optional(), // Dislike button callback action ID (default: "ai_res_dislike")
+  cardLikeVar: z.string().optional(), // Card variable name to set on like/dislike (default: "like")
   endpoint: z.string().optional(), // DWClient gateway endpoint
   debug: z.boolean().optional(), // DWClient debug mode
   enableMediaUpload: z.boolean().optional(),

DingTalk-Real-AI/dingtalk-moltbot-connector

src/config/schema.ts
@@ -80,6 +80,11 @@ const DingtalkSharedConfigShape = {
   groupSessionScope: GroupSessionScopeSchema,
   asyncMode: z.boolean().optional(),
   ackText: z.string().optional(),
+  cardTemplateId: z.string().optional(), // Custom AI Card template ID (default: built-in template)
+  cardTemplateKey: z.string().optional(), // Content variable name in the card template (default: "msgContent")
+  cardLikeActionId: z.string().optional(), // Like button callback action ID (default: "ai_res_like")
+  cardDislikeActionId: z.string().optional(), // Dislike button callback action ID (default: "ai_res_dislike")
+  cardFeedbackStatusKey: z.string().optional(), // Card variable name to set on like/dislike (default: "like")
   endpoint: z.string().optional(), // DWClient gateway endpoint
   debug: z.boolean().optional(), // DWClient debug mode
   enableMediaUpload: z.boolean().optional(),